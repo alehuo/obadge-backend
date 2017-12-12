a5a4c4db6818cb7782257b3a66142acdbd752558
alehuo <aleksi.huotala@helsinki.fi>
2017-12-12 20:16:06 +0200

    Modify seed

---
+++ README.md
@@ -0,0 +1,8 @@
+# Obadge-backend
+
+## Installation
+
+1. Clone the repo
+2. ```yarn``` to install dependencies.
+3. ```yarn migrate-db``` to migrate the database.
+4. ```yarn seed-db``` to seed the database, creating admin user account.
--- seeds/users.js
+++ seeds/users.js
@@ -2,11 +2,18 @@ exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
+
+      // Admin user
+      var adminEmail = "user1@email.com";
+      var adminPass = "HelloWorld";
+
+      console.log('Admin user created: %s::%s', adminEmail, adminPass);
+
       // Inserts seed entries
       return knex('users').insert([{
           id: 1,
-          email: 'user1@email.com',
-          password: 'abc123',
+          email: adminEmail,
+          password: adminPass,
           admin: 1
         },
         {
